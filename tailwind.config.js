5f60e0d5582ff1331b1a434457a024db42e4cdff
mms2209 <54174085+mms2209@users.noreply.github.com>
2025-09-30 17:35:27 +0400

    Updated package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
